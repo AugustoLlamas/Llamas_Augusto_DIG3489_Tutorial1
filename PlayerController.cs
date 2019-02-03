a331bcdc1e53319af3f30ba45b2fc540fdd25aea
AugustoLlamas <46631358+AugustoLlamas@users.noreply.github.com>
2019-02-03 18:18:58 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,58 +1,93 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
     public Text countText;
     public Text winText;
+    public Text scoreText;
+    public Text livesText;
+    public Text loseText;
 
     private Rigidbody rb;
     private int count;
+    private static int score;
+    private int lives;
 
     void Start()
 
     {
-        rb = GetComponent < Rigidbody > ();
+        rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText ();
-        winText.text = "";
+        score = 0;
+        lives = 3;
+        SetAllText();
+       winText.text = "";
+        loseText.text = "";
     }
     private void Update()
     {
         if (Input.GetKey("escape"))
             Application.Quit();
-        
+
     }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        float jump;
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump = 22.1f;
+        else
+            jump = 0;
+            
+        Vector3 movement = new Vector3(moveHorizontal, jump, moveVertical);
 
         rb.AddForce(movement * speed);
     }
 
-        void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-            SetCountText ();
+            score = score + 10;
+            SetAllText();
+
+        }
+
+        else if (other.gameObject.CompareTag("Foe"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 0;
+            score = score - 5;
+            lives = lives - 1; 
+            SetAllText();
+        
+        }
+        if (count == 11)
+        {
+            transform.position = new Vector3(21f, gameObject.transform.position.y, 3.0f);
         }
     }
-       void SetCountText ()
+    void SetAllText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 11)
+        countText.text = "Pick up No: " + count.ToString();
+        scoreText.text = "Score: " + score.ToString();
+        livesText.text = "Lives: " + lives.ToString();
+        if (count >= 22)
         {
-            winText.text = "You Win!";
+            winText.text = "Extra Crispy!";
         }
-       
+        else if (lives <= 0)
+        {
+            Destroy(gameObject);
+            loseText.text = "You got trashed";
+        }
 
     }
 }
-
